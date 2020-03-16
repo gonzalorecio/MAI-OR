904d5fc512536feefb7ffb6bc095b2ff22ff7a6b
Gonzalo Recio <gonzalo.recio@est.fib.upc.edu>
2020-03-16 14:20:27 +0100

    Add scripts

--- Lab1/main.m
+++ Lab1/main.m
@@ -9,19 +9,29 @@ imshowpair(I1,I2,'montage')
 %%
 % corners1 = detectFASTFeatures(I1,'MinContrast',0.1);
 % corners2 = detectFASTFeatures(I2,'MinContrast',0.1);
-corners1 = detectSURFFeatures(I1);
-corners2 = detectSURFFeatures(I2);
+pts1 = detectSURFFeatures(I1);
+pts2 = detectSURFFeatures(I2);
 J = insertMarker(I1,corners1,'circle');
 imshow(J)
 
-%% 
 
+%%
+[featuresOriginal, validPtsOriginal]  = extractFeatures(I1, keypoints1);
+[featuresDistorted,validPtsDistorted] = extractFeatures(I2, keypoints2);
 
+indexPairs = matchFeatures(featuresOriginal,featuresDistorted);
 
 
 %% For matlab goe 'fitgeotrans'
 
-T = transform_image_to_original(I1,I2,corners1, corners2);
+T = transform_image_to_original(I1, I2, pts1, pts2);
 % imshowpair(T, 'montage');
 % imshow(T)
 
+%% Example with affine transformation
+
+A = imread('pout.tif');
+tform = affine2d([1.5 0 0; -0.5 1 0; 0 0 1]);
+B = imwarp(I,tform);
+figure 
+imshow(B);
